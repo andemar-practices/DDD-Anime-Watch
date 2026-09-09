9c5eac2498b8dce5670da133743132555035b18f
Andemar <andemartinezdev@gmail.com>
2026-09-08 21:29:37 -0500

    Init project

---
+++ src/main/java/com/andemar/ddd/animewatch/DddAnimeWatchApplication.java
@@ -0,0 +1,13 @@
+package com.andemar.ddd.animewatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddAnimeWatchApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DddAnimeWatchApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/andemar/ddd/animewatch/DddAnimeWatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andemar.ddd.animewatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddAnimeWatchApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
